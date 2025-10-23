f7b6c9637446ccea5d39a45fb22aac575d87a0e7
Kaushik Prasad <kaushikprasad1659@gmail.com>
2025-10-23 12:13:54 +0530

    Route Link

---
+++ Routes/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+
+import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { PageNotFound } from './page-not-found/page-not-found';
+
+export const routes: Routes = [
+    {path:'about',component:About},
+    {path:'login',component:Login},
+    {path:'contact',component:Contact},
+    {path:'',component:Home},
+    {path:'**',component:PageNotFound}
+  
+];
