21e5f9ca307fba0bfd238426b42f713ab1497c7d
Kaushik Prasad <kaushikprasad1659@gmail.com>
2025-10-24 14:14:22 +0530

    Add Routes

--- Dynamic-Routes/src/app/app.routes.ts
+++ Dynamic-Routes/src/app/app.routes.ts
@@ -1,18 +1,21 @@
-
 import { Routes } from '@angular/router';
-import { About } from './about/about';
-import { Contact } from './contact/contact';
-import { Login } from './login/login';
-import { Home } from './home/home';
-import { Profile } from './profile/profile';
+import {Home} from './home/home';
+import {About} from './about/about';
+import {Contact} from './contact/contact';
+import {Login} from './login/login';
+import {Profile} from './profile/profile';
 import { PageNotFound } from './page-not-found/page-not-found';
 
 export const routes: Routes = [
-    {path:'about',component:About},
-    {path:'login',component:Login},
-    {path:'contact',component:Contact},
     {path:'',component:Home},
+    {path:'about',component:About},
+    {path:'contact',component:Contact},
+    {path:'login',component:Login},
     {path:'profile',component:Profile},
     {path:'**',component:PageNotFound}
-  
+
+
+
 ];
+
+
---
+++ Routes/src/app/profile/profile.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.html',
+  styleUrl: './profile.css'
+})
+export class Profile {
+
+
+  userName: string|null="";
+  constructor(private route:ActivatedRoute){
+    
+  }
+  ngOnInit():void{
+    // this.userName = this.route.snapshot.paramMap.get('name');    1. 1st way
+    // console.log(this.userName);
+
+
+     this.route.queryParams.subscribe(params => {
+      this.userName = params['name'];
+      console.log('Name from URL:', this.userName);
+    });
+
+  }
+  
+}
